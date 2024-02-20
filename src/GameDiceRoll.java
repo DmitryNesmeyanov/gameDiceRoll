6ca95087bc57e1a65f667f02cac5671200922870
DmitryNesmeyanov <quniosh@yandex.ru>
2024-02-20 19:57:24 +0300

    Нашел решение в интернете и добавил кол-во попыток.

--- src/GameDiceRoll.java
+++ src/GameDiceRoll.java
@@ -4,19 +4,21 @@ public class GameDiceRoll {
     public static void main(String[] args) {
         int a = 1;
         int b = 1;
+        var c = 1;
         Scanner x = new Scanner(System.in);
         System.out.println("Угадай число от 1 до 100");
         a = (int)(Math.random()*100+1);
         while (true){
             System.out.println("Введи число");
             b = x.nextInt();
+            c++;
             if (a>b){
                 System.out.println("Я сам в шоке, но, загаданное число больше, брат");
             }else if (a<b){
                 System.out.println("Не ожидал от тебя такого. Загаданное число меньше, брат");
 
             }else{
-                System.out.println("Ты угадал");
+                System.out.print("Ты угадал, кол-во попыток : "+ c);
                 break;
 
             }
